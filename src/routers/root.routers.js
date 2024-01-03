59c6ec9e8c122d52817030899a7b4d58fac020be
dxquyet16 <quyetcoder16@gmail.com>
2024-01-03 11:29:20 +0700

    done

--- src/routers/root.routers.js
+++ src/routers/root.routers.js
@@ -1,9 +1,8 @@
 import express from "express";
 import userRouters from "./user.routers.js";
-import { authPage, authUser } from "../middlewares/Auth.Middlewares.js";
 
 const rootRouters = express.Router();
 
-rootRouters.use("/user", authUser, authPage(["ADM","MOD"]), userRouters);
+rootRouters.use("/user", userRouters);
 
 export default rootRouters;
--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getListStudent } from "../controllers/user.controllers.js";
+import { authPage, authUser } from "../middlewares/Auth.Middlewares.js";
 
 const userRouters = express.Router();
 
-userRouters.get("/get-list-user", getListStudent)
+userRouters.get("/get-list-user", authUser, authPage(["ADM", "MOD"]), getListStudent)
 
 export default userRouters;
